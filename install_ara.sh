00b9181a8c6e2118745db558a0a0c778fe08a582
F4ASS <56490178+F4ASS@users.noreply.github.com>
2023-03-17 21:46:42 +0100

    Update install_ara.sh

--- install_ara.sh
+++ install_ara.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mv restart.* /etc/spotnik/
 mv modede*.* /etc/spotnik/
-chmod +x modedeffrm.py
-chmod +x modedef.py
+chmod +x /etc/spotnik/modedeffrm.py
+chmod +x /etc/spotnik/modedef.py
 mv *.wav /usr/share/svxlink/sounds/fr_FR/RRF/
 cp /usr/share/svxlink/events.d/local/Logic.tcl /usr/share/svxlink/events.d/local/Logic.tcl.old
 mv Logic.tcl /usr/share/svxlink/events.d/local/
